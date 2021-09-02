111245ff3a54d95618d83b29b949a66d46308cc4
--global <--global>
2021-09-01 21:45:07 -0300

    Modificada la plantilla base

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,10 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        Entradas
+                    </x-nav-link>
+                    <x-nav-link :href="url('salidas')" :active="request()->routeIs('dashboard')">
+                        Salidas
                     </x-nav-link>
                 </div>
             </div>
